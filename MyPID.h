dfb99eb5208e9e4c3580c3f1e64d4581b0fe5ab1
Jan Wielgus <jan.wielgus12@gmail.com>
2020-08-02 15:15:54 +0200

    One tab less in class declaration

--- MyPID.h
+++ MyPID.h
@@ -13,42 +13,42 @@
 
 class MyPID
 {
-	public:
-		MyPID(float deltaTime, float kP=0.0f, float kI=0.0f, float kD=0.0f, uint16_t Imax=0);
-		float updateController(float setPoint, float measurement);
-		float updateController(float newError);
-		void setParameters(float kP, float kI, float kD, uint16_t Imax);
-		void set_kP(float kP);
-		void set_kI(float kI);
-		void set_kD(float kD);
-		void set_Imax(uint16_t imax);
-		float get_kP();
-		float get_kI();
-		float get_kD();
-		uint16_t get_Imax();
-		void setDeltaTime(float deltaTime);
-		float getDeltaTime();
-		void reset();
+public:
+	MyPID(float deltaTime, float kP=0.0f, float kI=0.0f, float kD=0.0f, uint16_t Imax=0);
+	float updateController(float setPoint, float measurement);
+	float updateController(float newError);
+	void setParameters(float kP, float kI, float kD, uint16_t Imax);
+	void set_kP(float kP);
+	void set_kI(float kI);
+	void set_kD(float kD);
+	void set_Imax(uint16_t imax);
+	float get_kP();
+	float get_kI();
+	float get_kD();
+	uint16_t get_Imax();
+	void setDeltaTime(float deltaTime);
+	float getDeltaTime();
+	void reset();
 
-		void setupDerivativeLowPassFilter(float cutOffFrequency); // enables drivative LPF filter and set it up
-		void disableDerivativeLowPassFilter(); // disables derivative low-pass filter
-		
-		
-		
-	private:
-		float lastError;
-		float integral;
-		float deltaTime;
-		
-		// PID parameters:
-		float kP;
-		float kI;
-		float kD;
-		uint16_t Imax;
+	void setupDerivativeLowPassFilter(float cutOffFrequency); // enables drivative LPF filter and set it up
+	void disableDerivativeLowPassFilter(); // disables derivative low-pass filter
+	
+	
+	
+private:
+	float lastError;
+	float integral;
+	float deltaTime;
+	
+	// PID parameters:
+	float kP;
+	float kI;
+	float kD;
+	uint16_t Imax;
 
-		// Derivative filter parts:
-		bool enableDerivativeLPF_flag = false;
-		LowPassFilter<float> derivativeLPF; // derivative low-pass filter
+	// Derivative filter parts:
+	bool enableDerivativeLPF_flag = false;
+	LowPassFilter<float> derivativeLPF; // derivative low-pass filter
 };
 
 
